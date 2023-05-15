b4944700f6eb2bc802e8e0eed07f35c169b3e57e
jopefv <joaopedrofviana22@gmail.com>
2023-05-15 18:47:36 -0300

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarRoute, SideBtnWrap } from './SidebarElements'
+
+const Sidebar = ({isOpen,toggle}) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about' onClick={toggle}>Sobre</SidebarLink>
+          <SidebarLink to='discover' onClick={toggle}>Estagios</SidebarLink>
+          <SidebarLink to='services' onClick={toggle}>Services</SidebarLink>
+          <SidebarLink to='signup' onClick={toggle}>Cadastre-se</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='/signin'>Login</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
