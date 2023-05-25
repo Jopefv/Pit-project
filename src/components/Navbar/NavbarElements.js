ad025c148769707a58a232f5b6e6ff89485a589a
jopefv <joaopedrofviana22@gmail.com>
2023-05-24 22:53:31 -0300

    Some chnages a resolved a problem related to responsivity

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,22 +1,23 @@
-import styled from 'styled-components';
-import { Link as LinkR } from 'react-router-dom';
-import { Link as LinkS } from 'react-scroll';
+import styled from 'styled-components'
+import { Link as LinkR } from 'react-router-dom'
+import { Link as LinkS } from 'react-scroll'
 
 export const Nav = styled.nav`
-  background: ${({ scrollNav }) => (scrollNav ? '#000' : 'transparent')};
+  background: #000; //${({scrollNav}) => (scrollNav ? '#000' : 'transparent')};
   height: 80px;
-  display: flex;
+  //margin-top: -80px;
+  display:flex;
   justify-content: center;
   align-items: center;
   font-size: 1rem;
   position: sticky;
-  top: 0;
-  z-index: 10;
+  top:0;
+  z-index:10;
 
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
   }
-`;
+`
 
 export const NavbarContainer = styled.div`
   display: flex;
@@ -26,8 +27,7 @@ export const NavbarContainer = styled.div`
   width: 100%;
   padding: 0 24px;
   max-width: 1100px;
-`;
-
+`
 export const NavLogo = styled(LinkR)`
   color: #fff;
   justify-self: flex-start;
@@ -41,13 +41,12 @@ export const NavLogo = styled(LinkR)`
   @media screen and (max-width: 768px) {
     margin-left: -10px;
     align-self: center;
-    margin-right: 10px; /* Adiciona um espaçamento entre a imagem e o texto */
+    margin-right: 170px; /* Adiciona um espaçamento entre a imagem e o texto */
   }
 `;
-
 export const MobileIcon = styled.div`
   display: none;
-
+  
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
@@ -58,10 +57,9 @@ export const MobileIcon = styled.div`
     cursor: pointer;
     color: #fff;
   }
-`;
-
+`
 export const NavMenu = styled.ul`
-  display: flex;
+  display:flex;
   justify-content: center;
   list-style: none;
   text-align: center;
@@ -70,12 +68,10 @@ export const NavMenu = styled.ul`
   @media screen and (max-width: 768px) {
     display: none;
   }
-`;
-
+`
 export const NavItem = styled.li`
   height: 80px;
-`;
-
+`
 export const NavLinks = styled(LinkS)`
   color: #fff;
   display: flex;
@@ -86,10 +82,9 @@ export const NavLinks = styled(LinkS)`
   cursor: pointer;
 
   &.active {
-    border-bottom: 3px solid #edac5a;
+    border-bottom: 3px solid #edac5a
   }
-`;
-
+`
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
@@ -97,8 +92,7 @@ export const NavBtn = styled.nav`
   @media screen and (max-width: 768px) {
     display: none;
   }
-`;
-
+`
 export const NavBtnLink = styled(LinkR)`
   border-radius: 50px;
   background: #edac5a;
@@ -117,15 +111,15 @@ export const NavBtnLink = styled(LinkR)`
     background: #fff;
     color: #010606;
   }
-`;
+`
 
 export const Img = styled.img`
-  width: 30px; /* Ajuste o tamanho da imagem conforme necessário */
+  width: 100px; /* Ajuste o tamanho da imagem conforme necessário */
   height: auto;
   margin-right: 10px; /* Adiciona um espaçamento entre a imagem e o texto */
 
-  @media screen and (max-width: 768px) {
-    margin-right: 0; /* Remove o espaçamento entre a imagem e o texto */
+  @media screen and (max-width: 480px) {
+    margin-right: -10; /* Remove o espaçamento entre a imagem e o texto */
   }
 `;
 
@@ -133,7 +127,7 @@ export const NavBtnLinkRoute = styled(LinkR)`
   border-radius: 50px;
   white-space: nowrap;
   padding: 10px 22px;
-  color: white;
+  color: White;
   font-size: 16px;
   outline: none;
   border: none;
@@ -148,6 +142,6 @@ export const NavBtnLinkRoute = styled(LinkR)`
   }
 
   &.active {
-    border-bottom: 3px solid #edac5a;
+    border-bottom: 3px solid #edac5a
   }
-`;
+`
